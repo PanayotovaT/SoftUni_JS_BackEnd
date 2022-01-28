9977288fb9158d1afb16cebccdf42c31f636ac7c
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-29 01:24:20 +0200

    Added a middleware.

--- expressJSandTemplating/demo/catalog.js
+++ expressJSandTemplating/demo/catalog.js
@@ -3,21 +3,21 @@ const {Router} = require('express');
 const router = Router();
 
 
-router.get('/catalog/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Catalog');
 });
 
-router.get('/catalog/:productId', (req, res) => {
+router.get('/:productId', (req, res) => {
     res.send('Product Page');
     console.log(req.params);
 
 });
 
-router.get('/catalog/:productId/details', (req, res) => {
+router.get('/:productId/details', (req, res) => {
     res.send('Details page');
 });
 
-router.get('/catalog/:category/:productId', (req, res) => {
+router.get('/:category/:productId', (req, res) => {
     console.log(req.params);
     res.send('Product from category');
 });
--- expressJSandTemplating/demo/index.js
+++ expressJSandTemplating/demo/index.js
@@ -3,7 +3,11 @@ const catalogController = require('./catalog');
 
 const app = express();
 
-app.use(catalogController);
+app.use( (req, res, next) => {
+    console.log('>>>', req.method, req.url);
+    next();
+});
+app.use('/catalog', catalogController);
 
 app.get('/', (req, res) => {
 
