0b6a7046e737c93da0c3564932903258b97cd8d2
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-31 10:22:19 +0200

    Added front-end code.

--- expressJSandTemplating/cars/controllers/home.js
+++ expressJSandTemplating/cars/controllers/home.js
@@ -1,9 +1,8 @@
 module.exports = {
    
     async home(req, res) {
-        console.log(req.query);
         const cars = await req.storage.getAll(req.query);
         
-        res.render('index', { cars, title: 'Car Catalog' });
+        res.render('index', { cars, title: 'Car Catalog', query: req.query });
     }
 }
