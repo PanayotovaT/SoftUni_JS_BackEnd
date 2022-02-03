9ef82ba8335c38f2a4f2adb8dd2f2728050d5a2b
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-03 11:38:22 +0200

    Connected to the databse with the async functionality.

--- NoSQLandMongoDB/demo/index.js
+++ NoSQLandMongoDB/demo/index.js
@@ -1,15 +1,19 @@
-const { MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const connectionString = 'mongodb://localhost:27017';
-const connection = new MongoClient(connectionString, {
-    useUnifiedTopology: true
-});
+const connectionString = 'mongodb://127.0.0.1:27017';
 
-connection.connect((err, result) => {
-    if(err !== null) {
-        console.log('Database connection error!');
-        process.exit(1);
-    }
+start();
+
+async function start() {
+    const connection = new MongoClient(connectionString, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
+    
+    await connection.connect();
+ 
     console.log('Database connected');
-});
-
+    const db = connection.db('test');
+    const data =  await db.collection('cars').find({}).toArray();
+    console.log(data);
+}
