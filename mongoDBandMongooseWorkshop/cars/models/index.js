56ada87c44ac300ea665933b0be49a7e3510e0cb
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-06 11:34:47 +0200

    Car model created.

---
+++ mongoDBandMongooseWorkshop/cars/models/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Car = require('./Car');
+
+const connectionString = 'mongodb://127.0.0.1:27017/test';
+
+async function init() {
+
+    try{
+    await mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log('Database connected');
+    mongoose.connection.on('error', (err) => {
+        console.log('Database error.');
+        console.log(err);
+    }) 
+}catch(err) {
+    console.error('Error connecting to database');
+    process.exit(1);
+}
+}
+
+module.exports = init;
