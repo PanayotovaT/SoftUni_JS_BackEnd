b2a0c83ed07571745764e86373c7dc344f74d4c1
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-09 19:12:38 +0200

    Updated the session cookie demo.

--- sessionAndAuthentication/demo/demo.js
+++ sessionAndAuthentication/demo/demo.js
@@ -2,39 +2,49 @@ const express = require('express');
 
 const app = express();
 
-let visited = 0;
-let id;
+const sessions = {};
 
-app.get('/', (req, res) => {
+
+
+function mySession(req, res, next) {
     /* if(req.headers.cookie) {
-         console.log('>>>', req.headers.cookie);
-         visited =Number(req.headers.cookie.split('=')[1]);
-         visited++;
-     }
-     res.setHeader('Set-Cookie', `visited=${visited}; httpOnly`);
-     */
+        console.log('>>>', req.headers.cookie);
+        visited =Number(req.headers.cookie.split('=')[1]);
+        visited++;
+    }
+    res.setHeader('Set-Cookie', `visited=${visited}; httpOnly`);
+    */
     //  res.setHeader('Set-Cookie', `visited=${visited}; httpOnly`);
 
-  
-        const cookies = (req.headers.cookie || '')
-            .split(';')
-            .map(t => t.trim())
-            .map(t => t.split('='))
-            .reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
 
-        console.log('>>>', cookies);
+    const cookies = (req.headers.cookie || '')
+        .split(';')
+        .map(t => t.trim())
+        .map(t => t.split('='))
+        .reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
 
-        if (cookies.sessionId == undefined) {
-            const nextId = '000000' + (Math.random() * 999999).toString(16).slice(-6);
-            id = nextId;
-            res.setHeader('Set-Cookie', `sessionId=${nextId}; httpOnly`);
-            visited = 1;
-        } else if(cookies.sessionId == id) {
-            visited++;
+    console.log('>>>', cookies);
+
+   let user = sessions[cookies.sessionId];
+    if (user == undefined) {
+        const newId = '000000' + (Math.random() * 999999).toString(16).slice(-6);
+        user = {
+            visited: 1
         }
-    
+        sessions[newId] = user;
+        res.setHeader('Set-Cookie', `sessionId=${newId}; httpOnly`);
+    } else {
+        user.visited++;
+    }
+    req.session = user;
 
-    res.send(`<p>Hello</p><p>You have visisted this page ${visited} times.</p>`);
+next()
+}
+
+app.use(mySession)
+app.get('/', (req, res) => {
+
+    res.send(`<p>Hello</p><p>You have visisted this page ${req.session.visited} times.</p>`);
 })
 
 app.listen(3000);
