32c7e6ef77a1feaf2e3e91f62e2950dee14aeecc
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-09 11:20:13 +0200

    SessionId added with a visit counter.

--- sessionAndAuthentication/demo/demo.js
+++ sessionAndAuthentication/demo/demo.js
@@ -2,9 +2,39 @@ const express = require('express');
 
 const app = express();
 
+let visited = 0;
+let id;
+
 app.get('/', (req, res) => {
-    res.setHeader('Set-Cookie', 'theme=dark');
-    res.send('Hello');
+    /* if(req.headers.cookie) {
+         console.log('>>>', req.headers.cookie);
+         visited =Number(req.headers.cookie.split('=')[1]);
+         visited++;
+     }
+     res.setHeader('Set-Cookie', `visited=${visited}; httpOnly`);
+     */
+    //  res.setHeader('Set-Cookie', `visited=${visited}; httpOnly`);
+
+  
+        const cookies = (req.headers.cookie || '')
+            .split(';')
+            .map(t => t.trim())
+            .map(t => t.split('='))
+            .reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+
+        console.log('>>>', cookies);
+
+        if (cookies.sessionId == undefined) {
+            const nextId = '000000' + (Math.random() * 999999).toString(16).slice(-6);
+            id = nextId;
+            res.setHeader('Set-Cookie', `sessionId=${nextId}; httpOnly`);
+            visited = 1;
+        } else if(cookies.sessionId == id) {
+            visited++;
+        }
+    
+
+    res.send(`<p>Hello</p><p>You have visisted this page ${visited} times.</p>`);
 })
 
 app.listen(3000);
