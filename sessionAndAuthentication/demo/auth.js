f4585541c2e23f3450b835fdbc7a2f27f1f776e0
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-11 16:29:13 +0200

    created the auth middleware.

---
+++ sessionAndAuthentication/demo/auth.js
@@ -0,0 +1,37 @@
+const users = {
+    'sofi': {
+        username: 'sofi',
+        password: '123'
+    }
+};
+
+module.exports = () => {
+   return (req, res, next) => {
+        req.auth = {
+            login,
+            register
+        }
+
+        next();
+        function login(username, password) {
+            const user = users[username];
+            if (user && password == user.password) {
+                console.log('Sucecssfull Login');
+                req.session.user = user;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function register(username, password) {
+            const user = {
+                username: username,
+                password: password,
+            }
+            users[username] = user;
+            console.log('Register successfull', 'Welcome, ', username);
+            return true;
+        }
+    }
+}
--- sessionAndAuthentication/demo/demo.js
+++ sessionAndAuthentication/demo/demo.js
@@ -1,46 +1,40 @@
 const express = require('express');
-const expressSession =  require('express-session');
+const expressSession = require('express-session');
+const auth = require('./auth');
 
-const users = {
-    'sofi': {
-        username: 'sofi',
-        password:'123'
-    }
-}
+
 
 const app = express();
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(expressSession({
     secret: 'super secret',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: 'auto'}
-}))
-
+    cookie: { secure: 'auto' }
+}));
+app.use(auth());
 app.get('/', (req, res) => {
-    const user = req.session.user ||{
+
+    const user = req.session.user || {
         username: 'anonymous',
     };
-   console.log(req.session.user);
+    console.log(req.session.user);
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/login', (req, res) => {
+
     res.sendFile(__dirname + '/login.html');
 });
 
 app.post('/login', (req, res) => {
-    const user = users[req.body.username];
-    if(user && req.body.password==user.password){
-        console.log('Sucecssfull Login');
-        req.session.user = user;
+    if (req.auth.login(req.body.username, req.body.password)) {
+        res.redirect('/');
     } else {
         res.status(401).send('Incorrect username or password!')
     }
-
-    res.redirect('/')
 });
 
 app.get('/register', (req, res) => {
@@ -48,14 +42,11 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    console.log(req.body);
-    const user = {
-        username: req.body.username,
-        password: req.body.password,
+    if (req.auth.register(req.body.username, req.body.password)) {
+        res.redirect('/')
+    } else {
+        res.status(409).send('Username already exists!')
     }
-    user[req.session.username] = user;
-    console.log('Register successfull', req.session)
-    res.redirect('/')
 })
 
 app.listen(3000);
