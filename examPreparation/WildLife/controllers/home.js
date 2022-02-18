7c198b0e377b6bad9f36e460521a810e73ba20ba
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-18 11:49:48 +0200

    added diff. visibility to the btns in details.

--- examPreparation/WildLife/controllers/home.js
+++ examPreparation/WildLife/controllers/home.js
@@ -4,21 +4,30 @@ const { postViewModel } = require('../util/mappers');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Home Page'})
+    res.render('home', { title: 'Home Page' })
 })
 
 router.get('/catalog', async (req, res) => {
 
-    const posts  = (await getPosts()).map(postViewModel);
-    res.render('catalog', { title: 'Catalog Page', posts})
+    const posts = (await getPosts()).map(postViewModel);
+    res.render('catalog', { title: 'Catalog Page', posts })
 });
 
 router.get('/catalog/:id', async (req, res) => {
-   const id = req.params.id;
+    const id = req.params.id;
     const post = postViewModel(await getPostById(id));
-    console.log(post);
-  
-    res.render('details', { title: post.title, post})
+
+    if(req.session.user) {
+        post.hasUser = true;
+        if(req.session.user._id == post.author._id) {
+            post.isAuthor =  true;
+        }
+
+        post.hasVoted = false;
+        //TODO check votes
+    }
+
+    res.render('details', { title: post.title, post })
 })
 
 module.exports = router
