8866ccd83c664a9377a29cd8a51ecb7bfaba76b8
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-19 15:43:24 +0200

    Initialized the project.

---
+++ examPreparation/sharedTrip/controllers/auth.js
@@ -0,0 +1,56 @@
+const { Router } = require('express');
+const { isUser, isGuest } = require('../middleware/guards');
+const { register, login } = require('../services/user');
+const mapErrors = require('../util/mappers');
+
+const router = Router();
+
+router.get('/register', isGuest(),(req, res) => {
+    res.render('register', {title: 'Register Page'})
+});
+
+//TODO check form, action, method, field names
+router.post('/register', isGuest(), async (req, res) => {
+    try {
+        if (req.body.password != req.body.repeatPassword) {
+            throw new Error('Passwords don\'t match!');
+        }
+        const user = await register(req.body.username, req.body.password);
+        req.session.user = user;
+        res.redirect('/'); //TODO check redirect requirements
+    } catch (err) {
+        //TODO Send Error messages
+        const errors = mapErrors(err);
+        console.log(err);
+        res.render('register', {data: {username: req.body.username}, errors , title: 'Register Page'})
+    }
+})
+
+router.get('/login', isGuest(), (req, res) => {
+    res.render('login', {title: 'Login Page'});
+});
+
+//TODO check form action, method, field names
+router.post('/login', isGuest(), async (req, res) => {
+
+    try{
+        const user = await login(req.body.username, req.body.password);
+        req.session.user = user;
+        res.redirect('/')
+    } catch(err) {
+        const errors = mapErrors(err);
+        console.log(err);
+        //TODO Send Error messages
+        res.render('login', {data: {username: req.body.username}, errors, title: 'Login Page'})
+    }
+});
+
+router.get('/logout', isUser(), (req, res) => {
+    console.log(req.session.user)
+    delete req.session.user;
+    console.log(req.session.user)
+    res.redirect('/');
+    
+})
+
+module.exports = router;
