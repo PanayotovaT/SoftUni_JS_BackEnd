9f8a382d778496d08600a4345bfe4b9f88144b71
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-21 22:20:00 +0200

    create and catlog functionality added

--- restApiWorkshop/server/controllers/catalogController.js
+++ restApiWorkshop/server/controllers/catalogController.js
@@ -1,11 +1,31 @@
 const router  = require('express').Router();
+const furnitureService = require('../services/furniture');
 
-router.get('/', (req, res) => {
-    console.log('READ Catalog');
-    res.end();
+router.get('/', async (req, res) => {
+
+    const data = await furnitureService.getAll();
+    res.json(data);
+    
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const item = {
+        make: req.body.make,
+        model: req.body.model,
+        year: Number(req.body.year),
+        description: req.body.description,
+        price: Number(req.body.price),
+        img: req.body.img,
+        material: req.body.material
+
+    }
+
+    try{
+        const result  = await furnitureService.create(item);
+        res.status(201).json(result);
+    } catch(err) {
+        console.error(err.message);
+    }
     console.log('CREATE Catalog');
     console.log(req.body);
     res.end();
---
+++ restApiWorkshop/server/services/furniture.js
@@ -0,0 +1,38 @@
+const Item = require('../models/Item');
+
+async function getAll() {
+    return Item.find({});
+
+}
+
+async function getOne(id) {
+}
+
+async function getMyItems(userId) {
+
+}
+
+async function create(item) {
+    const result = new Item(item);
+    await result.save();
+
+    return result;
+}
+
+async function update(id, item){
+
+}
+
+async function deleteItem(id) {
+
+}
+
+module.exports = {
+    getAll,
+    getOne,
+    getMyItems,
+    create,
+    update,
+    deleteItem
+
+}
