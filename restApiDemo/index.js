940893754f8bff0dcf56e276966b5374d1d66677
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-21 12:38:24 +0200

    Cors added.

--- restApiDemo/index.js
+++ restApiDemo/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
+    next()
+})
+
 app.use(express.json());
 
 const data = [
