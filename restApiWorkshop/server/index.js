70ad84917c7791b2787a6b4e8fa15828ec753fcc
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-21 16:53:37 +0200

    Started the app and the database.

---
+++ restApiWorkshop/server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors  = require('./middlewares/cors');
+
+start();
+async function start() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/furniture', {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+
+        });
+        console.log('Database ready');
+    } catch (err) {
+        console.error('Database connection failed');
+        process.exit(1);
+    }
+
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+
+    app.get('/', (req, res) => res.json({message: 'REST service operational'}))
+    app.listen(3030, () => console.log('Rest Service started on port 3030'))
+    
+
+
+}
