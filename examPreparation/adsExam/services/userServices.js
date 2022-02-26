d299ada14a60644b8daeda283df98e8c6b9bb04a
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-02-26 14:55:50 +0200

    User Services add.

---
+++ examPreparation/adsExam/services/userServices.js
@@ -0,0 +1,45 @@
+const User = require('../models/User');
+const { hash, compare} = require('bcrypt');
+
+//TODO add all fields required by the exam
+async function register(email, password, skill) {
+    const existing = await getUserByEmail(email);
+    if(existing) {
+        throw new Error('Incorrect email or password!');
+    }
+
+    const hashedPassword = await hash(password, 10);
+    const user =  new User({
+        email,
+        hashedPassword,
+        skill
+    });
+
+    await user.save();
+    return user;
+}
+
+//TODO change identifier
+async function login(email, password) {
+    const user = await getUserByEmail(email);
+    if(!user) {
+        throw new Error('Incorrect email or password!')
+    }
+    const hasMatch = await compare(password, user.hashedPassword);
+    if(!hasMatch) {
+        throw new Error('Incorrect email or password');
+    }
+
+    return user;
+}
+
+//TODO identify user by given identifier
+async function getUserByEmail(email) {
+    const user =await User.findOne({ email: new RegExp(`^${email}$`, 'i') })
+    return user;
+}
+
+module.exports = {
+    login,
+    register
+}
