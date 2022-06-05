03c0ded73b5ee34af998d7d0b0b8fb1ba4600e79
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-06-05 23:24:04 +0300

    Added the controllers.

--- examPreparation/artGallery/config/routes.js
+++ examPreparation/artGallery/config/routes.js
@@ -1,5 +1,9 @@
+const authController = require('../controllers/auth');
 const homeController = require('../controllers/home');
+const galleryController = require('../controllers/gallery');
 
 module.exports = (app) => {
+    app.use(authController);
     app.use(homeController);
+    app.use(galleryController);
 }
