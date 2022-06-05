6b6c7d01e4f1448b209fdc0d68c3a9eae54aca50
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-06-05 22:21:47 +0300

    Added the database config.

---
+++ examPreparation/artGallery/config/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const databaseString = 'mongodb://127.0.0.1:27017/gallery';
+
+module.exports = async (app) => {
+
+    try{
+        await mongoose.connect(databaseString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('Database connected');
+
+        mongoose.connection.on('error' ,(error) => {
+            console.log('Database error');
+            console.log(error);
+        })
+
+    } catch(err) {
+        console.error('Error connecting to database!');
+        process.exit(1);
+    }
+
+
+}
--- examPreparation/artGallery/index.js
+++ examPreparation/artGallery/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const  = require('./config/express');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
 
 start();
 async function start() {
 
     const app = express();
     expressConfig(app);
+    await databaseConfig(app);
 
     app.listen(3000, () => {
         console.log('Server listening on port 3000');
