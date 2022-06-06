81491d6d861530419ecf5c13c362ad8862445d32
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-06-06 10:46:42 +0300

    Added galleryService.

--- examPreparation/artGallery/controllers/gallery.js
+++ examPreparation/artGallery/controllers/gallery.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 
+router.get('/gallery', (req, res) => {
+
+})
+
+
 
 module.exports = router;
 
---
+++ examPreparation/artGallery/services/galleryService.js
@@ -0,0 +1,46 @@
+const Publication = require('../models/Publication');
+
+async function getPublications() {
+    const publications = await Publication.find({}).lean();
+    return publications;
+}
+
+async function getPublication(id) {
+    const publication = await Publication.findById(id).lean();
+    return publication;
+}
+
+async function createPublication(publication) {
+    const newPublication = new Publication(publication);
+    await newPublication.save();
+    return newPublication;
+}
+
+async function updatePublication(id, publication) {
+    const existingPublication = await Publication.findById(id);
+
+    existingPublication.title = publication.title;
+    existingPublication.technique = publication.technique;
+    existingPublication.imageUrl = publication.imageUrl;
+    existingPublication.certificate = publication.certificate;
+
+    await existingPublication.save();
+
+}
+
+async function deletePublication(id) {
+    await Publication.findByIdAndDelete(id);
+}
+
+async function sharePublication(publicationId, userId) {
+
+}
+
+module.exports = {
+    getPublications,
+    getPublication,
+    createPublication,
+    updatePublication,
+    deletePublication,
+    sharePublication
+}
