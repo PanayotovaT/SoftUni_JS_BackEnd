8306d2b9a67dd5b1fb66ad664b9f53fb33a7b72a
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-09-20 23:34:10 +0300

    Ad controller added.

--- exam/jobAds/config/routes.js
+++ exam/jobAds/config/routes.js
@@ -1,8 +1,10 @@
 const homeController = require('../controllers/homeController');
+const adController = require('../controllers/adController');
 
 module.exports  = (app) => {
     app.use(homeController);
-    
+    app.use(adController);
+
     app.get('*', (req, res) => {
         res.render('404', { title: 'Page Not Found'})
     })
--- exam/jobAds/controllers/adController.js
+++ exam/jobAds/controllers/adController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/create', (req, res) {
+router.get('/create', (req, res)=> {
     res.render('create', {title: 'Create Page'});
 })
+
+module.exports = router;
