3183d4676d91d2506fe0a46d25a8e90ed049ede4
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-09-20 23:42:24 +0300

    Added error mappers.

--- exam/jobAds/controllers/adController.js
+++ exam/jobAds/controllers/adController.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
 
+const { create, deleteAd, update, applyToAd, getSearch } = require('../services/adServices');
+const mapErrors = require('../util/mappers');
+
 router.get('/create', (req, res)=> {
     res.render('create', {title: 'Create Page'});
+});
+
+router.post('/create', (req, res) => {
+    const ad =
+        {
+            headline: req.body.headline,
+            location: req.body.location,
+            company: req.body.company,
+            description: req.body.description,
+            author: req.session.user._id
+        };
+    try {
+
+    }catch(err){
+        console.log(err);
+        const errors = mapErrors(err);
+        res.render('create', {data: ad, errors , title: 'Create Page'});
+    }
+
 })
 
 module.exports = router;
