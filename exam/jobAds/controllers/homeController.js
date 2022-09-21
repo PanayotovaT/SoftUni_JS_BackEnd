8a177406c9c0cc5f1b30df1604db00585984310e
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-09-22 01:17:45 +0300

    Added details.

--- exam/jobAds/controllers/homeController.js
+++ exam/jobAds/controllers/homeController.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const { getAll } = require('../services/adServices');
+const { isUser } = require('../middlewares/guards');
+const preload =  require('../middlewares/preload');
+
 
 router.get('/', async (req, res) => {
     let ads = await getAll();
@@ -13,6 +16,23 @@ router.get('/ads', async (req, res) => {
     let ads =  await getAll();
 
     res.render('ads', {title: 'All Advertisements', ads})
+});
+
+router.get('/details/:id', isUser(), preload(), async (req, res) => {
+    const ad = res.locals.ad;
+    const userId = req.session.user?._id;
+
+    res.locals.isOwner = ad.author._id == userId;
+
+    if(ad.candidates.some(x => x._id == userId)) {
+        res.locals.hasApplied = true;
+    }
+
+    res.locals.candidates = ad.candidates;
+    res.locals.candidatesNum = ad.candidates.length;
+    
+    res.render('details', {title: 'Details Page'});
+
 })
 
 module.exports = router;
